03d6815268b4ca58ebdf4353177bcf6126ee55bb
WWW_cool <WWW_cool@inbox.ru>
2019-11-17 23:43:00 +0300

    added tail glow + zoom camera

--- Assets/Scripts/Game/TailController.cs
+++ Assets/Scripts/Game/TailController.cs
@@ -6,9 +6,11 @@ namespace UnityPrototype
 {
     public class TailController : MonoBehaviour
     {
+        [SerializeField] private Animator m_animator;
 
         private void OnEnable()
         {
+            m_animator.Play("Idle");
             Destroy(gameObject, TailSpawnManager.s_destroyTime);
         }
     }
